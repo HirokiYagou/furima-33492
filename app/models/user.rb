95e5c256bed97f89f5f83265ca6cc08b4b05172d
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2020-12-24 22:51:38 +0900

    新規登録実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  
+  EMAIL_REGEX = /\A\S+@\S+\.\S+\z/.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+  validates :email,    format: {with: EMAIL_REGEX,    message: "には@を含めてください"}
+  validates :password, format: {with: PASSWORD_REGEX, message: "には英字と数字の両方を含めて設定してください"}
+  validates :last_name_kana,  presence: true, format: {with: KANA_REGEX, message: "全角カタカナで入力してください"}
+  validates :first_name_kana, presence: true, format: {with: KANA_REGEX, message: "全角カタカナで入力してください"}
+  validates :nickname,        presence: true
+  validates :last_name,       presence: true
+  validates :first_name,      presence: true
+  validates :birthday,        presence: true
 end
