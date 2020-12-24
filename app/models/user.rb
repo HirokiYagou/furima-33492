36b4c3d0c7de3b9c4becd1362fea77c858f25d9c
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2020-12-24 23:15:39 +0900

    rspec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
   EMAIL_REGEX = /\A\S+@\S+\.\S+\z/.freeze
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
-  validates :email,    format: {with: EMAIL_REGEX,    message: "には@を含めてください"}
-  validates :password, format: {with: PASSWORD_REGEX, message: "には英字と数字の両方を含めて設定してください"}
-  validates :last_name_kana,  presence: true, format: {with: KANA_REGEX, message: "全角カタカナで入力してください"}
-  validates :first_name_kana, presence: true, format: {with: KANA_REGEX, message: "全角カタカナで入力してください"}
+  validates :email,    format: { with: EMAIL_REGEX,    message: 'には@を含めてください' }
+  validates :password, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
+  validates :last_name_kana,  presence: true, format: { with: KANA_REGEX, message: '全角カタカナで入力してください' }
+  validates :first_name_kana, presence: true, format: { with: KANA_REGEX, message: '全角カタカナで入力してください' }
   validates :nickname,        presence: true
   validates :last_name,       presence: true
   validates :first_name,      presence: true
