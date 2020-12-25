b07b1b7974223226b124fc1915ef1ac4b20705b9
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2020-12-25 12:41:55 +0900

    バリデーションとアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :condition_id
+    validates :cost_id
+    validates :area_id
+    validates :days_id
+    validates :price
+  end
+
+  belongs_to :user
 end
