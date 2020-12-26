a0aaf374bb66532a2eaa09ca5b306803a56bb47b
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2020-12-26 18:07:59 +0900

    updateがユーザーと出品者が同じでないと実行されない実装、及びcreateとupdateがログインユーザーでないとできない実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -24,15 +25,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @item.user.id
-      redirect_to action: :index
-    end
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to item_path(item)
+    if @item.update(item_params)
+      redirect_to item_path(@item)
     else
       render :edit
     end
@@ -55,5 +52,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user.id == @item.user.id
+      redirect_to action: :index
+    end
   end
 end
