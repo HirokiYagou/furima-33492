bab5452ea382e87f40d1894db095b9703101a1e6
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2020-12-27 17:02:12 +0900

    商品詳細ページから購入ページへのリンク作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   def index
-    
+    @order_shipping = OrderShipping.new
   end
 
   def create
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -4,12 +4,14 @@ class OrderShipping
 
   with_options presence: true do
     validates :post
-    validates :area, numericality: { other_than: 1 }
+    validates :area_id, numericality: { other_than: 1 }
     validates :city
     validates :address
     validates :tel, format: { with: /\A0\d{9,10}\z/.freeze, message: 'は0から始まる10桁または11桁の半角数字で入力してください' }
   end
 
   def save
+    order = Order.create(item_id: item.id, user_id: user.id)
+    Shipping.create(post: post, area_id: area_id, city: city, address: address, tel: tel)
   end
 end
