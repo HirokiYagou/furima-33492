22c4972c42c2c2a0f7d2a078d4b40c4472dfd43c
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2020-12-28 19:29:01 +0900

    一つのif文を二つに分ける（リファクタリング）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -49,8 +49,11 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    if @item.order || current_user.id == @item.user.id
-      redirect_to root_path
+    if @item.order
+      return redirect_to root_path
+    end
+    if current_user.id == @item.user.id
+      return redirect_to root_path
     end
   end
 end
