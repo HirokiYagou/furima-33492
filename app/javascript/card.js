1741d1a42a802a45d9efcf2f02a0c0a0d1310043
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-07 21:54:08 +0900

    バグ修正の変更点を元に戻す

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -20,8 +20,6 @@ const pay = () => {
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type='hidden'> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-        console.log(token);
-        console.log('test');
       }
 
       document.getElementById("card-number").removeAttribute("name");
@@ -29,8 +27,7 @@ const pay = () => {
       document.getElementById("card-exp-month").removeAttribute("name");
       document.getElementById("card-exp-year").removeAttribute("name");
 
-      console.log(token);
-      // document.getElementById("charge-form").submit();
+      document.getElementById("charge-form").submit();
     });
   });
 };
