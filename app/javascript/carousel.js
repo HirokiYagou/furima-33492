997aab3c0656b0c5d38ffb6addc4e4a65963c0a3
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-17 13:50:54 +0900

    画像遷移実装

--- app/javascript/carousel.js
+++ app/javascript/carousel.js
@@ -2,8 +2,40 @@ function carousel() {
   const app = Vue.createApp({
     data() {
       return {
-        message: 'たまプラーザ'
+        message: 'たまプラーザ',
+        bars: [
+          {
+            id: 1,
+            src: "/assets/bar1.jpg"
+          },
+          {
+            id: 2,
+            src: "/assets/bar2.png"
+          }
+        ],
+        selectedIndex: 1,
+        imageTransitionName: "prev",
       }
+    },
+    computed: {
+      target: function() {
+        const self = this
+        return this.bars[self.selectedIndex]
+      },
+      lastIndex: function() {
+        return this.bars.length - 1
+      }
+    },
+    methods: {
+      onClickPager: function(index) {
+        this.imageTransitionName = this.selectedIndex < index ? "next" : "prev"
+        this.selectedIndex = index
+      },
+      nextPager: function() {
+        this.imageTransitionName = "next"
+        this.selectedIndex =
+          this.selectedIndex >= this.lastIndex ? 0 : this.selectedIndex + 1
+      },
     },
   })
 
