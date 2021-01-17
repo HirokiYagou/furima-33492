0d0184606b0a31f97c96c805bb01a24f7052a450
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-17 18:19:44 +0900

    Update price.js

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,7 +1,9 @@
-window.addEventListener("keyup", () => {
-  const itemPrice = document.getElementById("item-price");
-  const addTaxPrice = document.getElementById("add-tax-price");
-  const profit = document.getElementById("profit");
-  addTaxPrice.textContent = Math.floor(itemPrice.value * 0.1);
-  profit.textContent = itemPrice.value - Math.floor(itemPrice.value * 0.1);
-});
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  window.addEventListener("keyup", () => {
+    const itemPrice = document.getElementById("item-price");
+    const addTaxPrice = document.getElementById("add-tax-price");
+    const profit = document.getElementById("profit");
+    addTaxPrice.textContent = Math.floor(itemPrice.value * 0.1);
+    profit.textContent = itemPrice.value - Math.floor(itemPrice.value * 0.1);
+  });
+}
